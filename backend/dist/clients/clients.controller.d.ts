fdf5841a0c6fbd750dacad32cf253b5a57b5453a
Yulman <yulmanbryan009@hotmail.com>
2025-05-05 18:56:25 -0500

    se añade dist a ignore

--- backend/dist/clients/clients.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ClientsService } from './clients.service';
-export declare class ClientsController {
-    private readonly clientsService;
-    constructor(clientsService: ClientsService);
-    findAll(): Promise<import("../entities/client.entity").Client[]>;
-    findOne(id: number): Promise<import("../entities/client.entity").Client | null>;
-    create(body: any): Promise<import("../entities/client.entity").Client>;
-}
