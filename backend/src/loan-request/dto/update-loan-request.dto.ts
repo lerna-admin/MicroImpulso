d769d4b8a2d05b43d7cb89966139e0149a815111
Yulman <yulmanbryan009@hotmail.com>
2025-05-12 08:39:02 -0500

    Se elimina el backend para usarse desde otro repo

--- backend/src/loan-request/dto/update-loan-request.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLoanRequestDto } from './create-loan-request.dto';
-
-export class UpdateLoanRequestDto extends PartialType(CreateLoanRequestDto) {}
