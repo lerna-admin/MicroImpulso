a0b7d32659338a21212f9c84894e5b0cbbef4090
henry8812@gmail.com <henry8812@gmail.com>
2025-05-22 05:22:06 +0000

    refresh

--- src/app/dashboard/chat/[threadType]/[threadId]/page.js
+++ src/app/dashboard/chat/[threadType]/[threadId]/page.js
@@ -2,6 +2,8 @@ import * as React from "react";
 
 import { appConfig } from "@/config/app";
 import { ThreadView } from "@/components/dashboard/chat/thread-view";
+import { useAutoRefresh } from "@/app/dashboard/chat/hooks/use-auto-refresh""
+
 
 export const metadata = { title: `Thread | Chat | Dashboard | ${appConfig.name}` };
 
@@ -9,6 +11,7 @@ export default async function Page({ params }) {
 	const { threadId, threadType } = await params;
 
 
+	useAutoRefresh(20000)
 
 	return <ThreadView threadId={threadId} threadType={threadType} />;
 }
