37db6ed2058d5692fadad7509f1b93dd58e7e397
Yulman <yulmanbryan009@hotmail.com>
2025-05-24 00:57:46 -0500

    se agrega el image para next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 const nextConfig = {
-  // Next.js will ignore ESLint errors during builds.
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+	// Next.js will ignore ESLint errors during builds.
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 
-  // (Optional) You can add other global settings here
-  // For example, enable React Strict Mode
-  reactStrictMode: true,
+	images: {
+		remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_API_URL}/documents/**/file`)],
+	},
 
-  // You can also define custom webpack behavior here if needed
+	// (Optional) You can add other global settings here
+	// For example, enable React Strict Mode
+	reactStrictMode: true,
+
+	// You can also define custom webpack behavior here if needed
 };
 
 module.exports = nextConfig;
